475ea91ab1a7c372fb31c51e2f13071dd6eae429
Joao Wictor <119543998+WictorPereira@users.noreply.github.com>
2024-01-15 14:29:20 -0300

    Estruturando o projeto
    Add= layout, uso de props no container

--- costs/src/App.jsx
+++ costs/src/App.jsx
@@ -1,11 +1,38 @@
-function App() {
-  
+import { BrowserRouter as Router, Route, Link, Routes } from'react-router-dom'
+import Home from '../src/components/pages/Home'
+import Company from '../src/components/pages/Company'
+import Contact from '../src/components/pages/Contact'
+import NewProject from '../src/components/pages/NewProject'
+import Container from './components/layout/Container.jsx'
 
+
+
+function App() {
   return (
-   <div className='App'>
-      <h1>Costs</h1>
-   </div>
+    
+   <Router>
+    
+    <div>
+      <Link to="/">Home</Link>
+      <Link to="/Contact">Contato</Link>
+      <Link to="/Company">Empresa</Link>
+      <Link to="/NewProject">Novo projeto</Link>
+    </div>
+    <Container customClass="min-height">
+    <Routes> 
+      <Route path='/' element={<Home/>}/>
+      <Route path='/company' element={<Company/>}/>
+      <Route path='/contact' element={<Contact/>}/>
+      <Route path='/newproject' element={<NewProject/>}/>
+    </Routes>
+    </Container>
+    <p>Footer</p>
+   
+   </Router>
+
+   
   );
+
 }
 
 export default App
