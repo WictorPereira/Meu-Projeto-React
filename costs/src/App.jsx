b385bd9552ca42e112436dcd817ef8572519b4bd
Joao Wictor <119543998+WictorPereira@users.noreply.github.com>
2024-01-16 16:19:30 -0300

    Navbar e Footer
     Navbar e Footer  jsx e css concluídos .layout deles adicionados no App.jsx .

--- costs/src/App.jsx
+++ costs/src/App.jsx
@@ -3,7 +3,12 @@ import Home from '../src/components/pages/Home'
 import Company from '../src/components/pages/Company'
 import Contact from '../src/components/pages/Contact'
 import NewProject from '../src/components/pages/NewProject'
+import Projects from './components/pages/Projects.jsx'
+
 import Container from './components/layout/Container.jsx'
+import Navbar from './components/layout/Navbar.jsx'
+import Footer from './components/layout/Footer.jsx'
+
 
 
 
@@ -11,22 +16,17 @@ function App() {
   return (
     
    <Router>
-    
-    <div>
-      <Link to="/">Home</Link>
-      <Link to="/Contact">Contato</Link>
-      <Link to="/Company">Empresa</Link>
-      <Link to="/NewProject">Novo projeto</Link>
-    </div>
+    <Navbar/>
     <Container customClass="min-height">
     <Routes> 
       <Route path='/' element={<Home/>}/>
       <Route path='/company' element={<Company/>}/>
       <Route path='/contact' element={<Contact/>}/>
       <Route path='/newproject' element={<NewProject/>}/>
+      <Route path='/projects' element={<Projects/>}/>
     </Routes>
     </Container>
-    <p>Footer</p>
+    <Footer/>
    
    </Router>
 
