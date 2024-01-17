c331d21df203f05001708ffa0f083a3780db042f
Joao Wictor <119543998+WictorPereira@users.noreply.github.com>
2024-01-17 17:25:25 -0300

    Componentização de formulário
    parte visual e dinâmica do formulário finalizada. Todos componentes do formulário estão na pasta form.

--- costs/src/components/project/ProjectForm.jsx
+++ costs/src/components/project/ProjectForm.jsx
@@ -1,24 +1,27 @@
 import styles from './ProjectForm.module.css'
 
-function ProjectForm() {
+import Input from '../form/Input.jsx'
+import Select from './../form/Select';
+import SubmitButton from '../form/SubmitButton.jsx';
+
+function ProjectForm( {btnText}) {
     return (
-        <form>
-            <div>
-                <input type="text" placeholder='Insira o nome do projeto'/>
-            </div>
-            <div>
-                <input type="number" placeholder='Insira o orçamento total'/>
-            </div>
-            <div>
-                <select name="category_id">
-                  <option disabled selected> 
-                  Selecione a categoria
-                  </option>
-            </select>
-            </div>
-            <div>
-                <input type="submit" value="Criar projeto" />
-            </div>
+        <form className={styles.form}>
+            <Input 
+              type="text" 
+              text="Nome do projeto"
+              name="name"
+              placeholder="Insira o nome do projeto" />
+            <Input 
+              type="number" 
+              text="Orçamento"
+              name="budget"
+              placeholder="Insira o Orçamento total" />
+            
+            <Select name="category_id" 
+            text="Criar projeto"/>
+
+            <SubmitButton text={btnText}/>
         </form>
     )
 }
