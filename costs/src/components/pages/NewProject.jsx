a138297ba24f1bcd776d5cb4796e3317893dae62
Joao Wictor <119543998+WictorPereira@users.noreply.github.com>
2024-01-17 16:11:08 -0300

    Inicialização do formulario
    jsx e css inicializados. pasta criada:(Projects) para apenas componentes de projetos ex:formulario

--- costs/src/components/pages/NewProject.jsx
+++ costs/src/components/pages/NewProject.jsx
@@ -1,5 +1,14 @@
+import styles from './NewProject.module.css'
+import ProjectForm from'../project/ProjectForm.jsx'
+
 function NewProject() {
-    return <h1>Novo projeto</h1>
+    return (
+        <div className={styles.newproject_container}>
+            <h1>Criar Projeto</h1>
+            <p>Crie seu projeto para depois acicionar os serviços</p>
+            <ProjectForm/>
+        </div>
+    )
 }
 
 export default NewProject
