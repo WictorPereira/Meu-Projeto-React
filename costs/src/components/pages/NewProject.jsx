79de6996aded19b3f7ba8c3ecca64789da9dfbf9
Joao Wictor <119543998+WictorPereira@users.noreply.github.com>
2024-01-18 17:27:39 -0300

    BD do formulário Funcionando
    Inserir dados no banco via API.

--- costs/src/components/pages/NewProject.jsx
+++ costs/src/components/pages/NewProject.jsx
@@ -1,12 +1,38 @@
 import styles from './NewProject.module.css'
 import ProjectForm from'../project/ProjectForm.jsx'
 
+import { useNavigate } from 'react-router-dom'
+
 function NewProject() {
+
+    const history = useNavigate()
+
+    function createPost (project) {
+        
+        //inicializaçao
+        project.cost = 0
+        project.servises = []
+
+        fetch("http://localhost:5000/projects",{
+            method:"POST",
+            headers:{
+                'Content-type':'application/json'
+            },   
+            body:JSON.stringify(project)       
+        })
+        .then((resp) => resp.json)
+        .then(data => {console.log(data)
+            //redirect
+        history('/projects',{message:'projeto criado com suceso'})
+    })       
+        .catch(err => console.log(err))   
+    }
+
     return (
         <div className={styles.newproject_container}>
             <h1>Criar Projeto</h1>
             <p>Crie seu projeto para depois acicionar os serviços</p>
-            <ProjectForm btnText= "Criar Projeto"/>
+            <ProjectForm handleSubmit={createPost} btnText= "Criar Projeto"/>
         </div>
     )
 }
