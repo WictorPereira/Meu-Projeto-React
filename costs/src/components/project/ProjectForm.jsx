93399d66708293f8d5f9d5e6cb7f993107b86473
Joao Wictor <119543998+WictorPereira@users.noreply.github.com>
2024-01-18 16:05:50 -0300

    Conexão com api/Banco de dados
    criação de um uma api que simula um BD para coletar dinamicamente categorias no Select do formulario .

--- costs/src/components/project/ProjectForm.jsx
+++ costs/src/components/project/ProjectForm.jsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react';
+
 import styles from './ProjectForm.module.css'
 
 import Input from '../form/Input.jsx'
@@ -5,6 +7,22 @@ import Select from './../form/Select';
 import SubmitButton from '../form/SubmitButton.jsx';
 
 function ProjectForm( {btnText}) {
+  const [categories,setCategories] = useState ([])
+
+  useEffect(()=>{
+    fetch("http://localhost:5000/categories",{
+    method: "GET",
+    headers:{
+      'Content-Type':'application/json'
+    }
+  })
+    .then((resp)=> resp.json())
+    .then((data) => {
+      setCategories(data)
+    })
+    .catch((err)=>console.log(err))
+  },[])
+  console.log('Categories:', categories);
     return (
         <form className={styles.form}>
             <Input 
@@ -18,8 +36,11 @@ function ProjectForm( {btnText}) {
               name="budget"
               placeholder="Insira o Orçamento total" />
             
-            <Select name="category_id" 
-            text="Criar projeto"/>
+            <Select 
+            name="category_id" 
+            text="Selecione a categoria"
+            options={categories}
+            />
 
             <SubmitButton text={btnText}/>
         </form>
